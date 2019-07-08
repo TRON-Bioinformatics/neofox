ab83cde0a5407bb44b22a0e603e69f01cff6b0e0
Lang <franlang@tronc5.zdv.uni-mainz.de>
2019-07-08 16:30:10 +0200

    minor correction in dai function

--- FeatureLiterature.py
+++ FeatureLiterature.py
@@ -36,8 +36,9 @@ def dai(props, mhc, multiple_binding=False, affinity = False):
             sc_mut = props["MB_score_top10_harmonic"]
             sc_wt = props["MB_score_WT_top10_harmonic"]
         elif affinity:
-            sc_mut = props["best_affinity_epitope_netmhcpan4"]
+            sc_mut = props["best_affinity_netmhcpan4"]
             sc_wt = props["best_affinity_netmhcpan4_WT"]
+            print >> sys.stderr, sc_mut, sc_wt
         else:
             sc_mut = props["MHC_I_score_.best_prediction."]
             sc_wt = props["MHC_I_score_.WT."]
