dfeb6c951f63854ccabcfca7fd6493ed29453181
Lang <franlang@tronp.zdv.uni-mainz.de>
2019-08-19 09:37:55 +0200

    add function to calculate MB best per allele/PHBR-I only if 6 alleles typed and consider homozygosity here

--- netmhcpan4/combine_netmhcpan_pred_multiple_binders.py
+++ netmhcpan4/combine_netmhcpan_pred_multiple_binders.py
@@ -66,7 +66,17 @@ class Bestandmultiplebinder:
         self.mhcI_affinity_epitope_9mer_WT = "NA"
 
 
-
+    def MHC_MB_score_best_per_allele(self,tuple_best_per_allele):
+        '''returns list of multiple binding scores for mhcII considering best epitope per allele, applying different types of means (harmonic ==> PHRB-II, Marty et al).
+        2 copies of DRA - DRB1 --> consider this gene 2x when averaging mhcii binding scores
+        '''
+        number_alleles = len(tuple_best_per_allele)
+        multbind = multiple_binders.MultipleBinding()
+        tuple_best_per_allele_new = list(tuple_best_per_allele)
+        if len(tuple_best_per_allele_new) == 6:
+            return multbind.wrapper_mean_calculation(tuple_best_per_allele_new)
+        else:
+            return ["NA", "NA", "NA"]
 
     def main(self, epi_dict, patient_hlaI, set_available_mhc):
         '''predicts MHC epitopes; returns on one hand best binder and on the other hand multiple binder analysis is performed
@@ -102,7 +112,7 @@ class Bestandmultiplebinder:
         self.MHC_score_top10 = mb.wrapper_mean_calculation(top10)
         best_per_alelle = mb.scores_to_list(best_per_alelle)
         self.MHC_score_all_epitopes = mb.wrapper_mean_calculation(all)
-        self.MHC_score_best_per_alelle = mb.wrapper_mean_calculation(best_per_alelle)
+        self.MHC_score_best_per_alelle = self.MHC_MB_score_best_per_allele(best_per_alelle)
         self.MHC_number_strong_binders = mb.determine_number_of_binders(all, 1)
         self.MHC_number_weak_binders = mb.determine_number_of_binders(all, 2)
         # best prediction
@@ -202,8 +212,11 @@ if __name__ == '__main__':
     #file = "/projects/CM01_iVAC/immunogenicity_prediction/3rd_party_solutions/INPuT/nonprogramm_files/test_fulldat.txt"
     #hla_file = "/projects/CM01_iVAC/immunogenicity_prediction/3rd_party_solutions/indels/RB_0004_labHLA_V2.csv"
     #hla_file = "/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/riaz/output_tables_pre/alleles.csv"
-    file = "/projects/CM01_iVAC/immunogenicity_prediction/3rd_party_solutions/MHC_prediction_netmhcpan4/testdat_ott.txt"
-    hla_file ="/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/ott/icam_ott/alleles.csv"
+    file = "/projects/CM01_iVAC/immunogenicity_prediction/3rd_party_solutions/MHC_prediction_netmhcpan4/test_ott_head_pt10.txt"
+    #hla_file ="/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/ott/icam_ott/alleles.csv"
+    hla_file = "/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/ott/icam_ott/20190819_alleles_extended.csv"
+    #hla_file = "/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/hugo/icam_hugo/20190816_alleles_extended.csv"
+    #file = "/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/hugo/icam_hugo/Pt12/scratch/Pt12_mut_set.txt.transcript.squish.somatic.freq"
     dat = data_import.import_dat_icam(file, False)
     if "+-13_AA_(SNV)_/_-15_AA_to_STOP_(INDEL)" in dat[0]:
         dat = data_import.change_col_names(dat)
@@ -221,13 +234,14 @@ if __name__ == '__main__':
             dict_epi.init_properties(dat[0], dat[1][ii])
             x =  Bestandmultiplebinder()
             x.main(dict_epi.properties, patient_hlaI, set_available_mhc)
+            print dict_epi.properties["patient.id"]
             #print x.MHC_epitope_scores_WT
             #print x.MHC_epitope_seqs_WT
             #print x.MHC_epitope_seqs
             attrs = vars(x)
             #print attrs
             print x.mhcI_affinity_epitope_9mer
-            print x.mhcI_affinity_epitope_9mer_WT
+            print x.MHC_score_best_per_alelle
             #print "score"
             #print x.best4_affinity_epitope
             #print x.best4_affinity_epitope_WT
