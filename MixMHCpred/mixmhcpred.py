b4357f2720f45d72c10b7074bfe120c097eb123d
Lang <franlang@tronc3.zdv.uni-mainz.de>
2019-10-23 11:02:17 +0200

    determination pos of mutation in long seq for wt and mut not same length

--- MixMHCpred/mixmhcpred.py
+++ MixMHCpred/mixmhcpred.py
@@ -35,9 +35,17 @@ class MixMHCpred:
         xmer_wt = props["X.WT._..13_AA_.SNV._._.15_AA_to_STOP_.INDEL."]
         xmer_mut = props["X..13_AA_.SNV._._.15_AA_to_STOP_.INDEL."]
         p1 = -1
-        for i,aa in enumerate(xmer_mut):
-            if aa != xmer_wt[i]:
-                p1 = i + 1
+        if len(xmer_wt) == len(xmer_mut):
+            p1 = -1
+            for i,aa in enumerate(xmer_mut):
+                if aa != xmer_wt[i]:
+                    p1 = i + 1
+        else:
+            p1 = 0
+            # in case sequences do not have same length
+            for a1, a2 in zip(xmer_wt, xmer_mut):
+                if a1 == a2:
+                    p1 += 1
         return str(p1)
 
     def generate_nmers(self, props, list_lengths, mut = True ):
