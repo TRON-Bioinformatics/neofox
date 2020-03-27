bed40b8896f096606d3b5a7396e70af56419e5f6
Pablo Riesgo Ferreiro <pablo.riesgoferreiro@tron-mainz.de>
2020-03-27 08:00:34 +0100

    set a reference folder through an env variable

---
+++ input/exceptions.py
@@ -0,0 +1,16 @@
+
+
+class INPuTInputParametersException(ValueError):
+    pass
+
+
+class INPuTConfigurationException(ValueError):
+    pass
+
+
+class INPuTCommandException(ValueError):
+    pass
+
+
+class INPuTReferenceException(ValueError):
+    pass
