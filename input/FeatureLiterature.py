bc1cca2200147507524f472e968e87d38d470a0d
Lang <franlang@tronc6.zdv.uni-mainz.de>
2020-04-13 17:42:25 +0200

    modify mutated epxression function to consider if vaf in rna should be used; NOT CORRECT YET

--- input/FeatureLiterature.py
+++ input/FeatureLiterature.py
@@ -105,15 +105,26 @@ def ratio_number_binders(props, mhc = MHC_I, threshold = 1):
     except ValueError:
         return "NA"
 
-def rna_expression_mutation(props):
+def rna_expression_mutation(props, rna_avail):
     '''
     This function calculates the product of VAF in RNA and transcript expression
     to reflect the expression of the mutated transcript
     '''
     transcript_expression = props["transcript_expression"]
+    if "patient.id" in props:
+        patid = props["patient.id"]
+    else:
+        patid = props["patient"]
     try:
-        vaf_rna = props["VAF_in_RNA"]
-    except KeyError:
+        rna_avail =  rna_avail[patid]
+    except (KeyError, ValueError) as e:
+        rna_avail = "NA"
+    if rna_avail or rna_avail == "NA":
+        try:
+            vaf_rna = props["VAF_in_RNA"]
+        except KeyError:
+            vaf_rna = props["VAF_in_tumor"]
+    elif rna_avail == False:
         vaf_rna = props["VAF_in_tumor"]
     try:
         return str(float(transcript_expression) * float(vaf_rna)) if float(vaf_rna) > 0 else "NA"
