f1a14e838ff257d1399ee8b51ff0043651750fc6
Pablo Riesgo Ferreiro <pablo.riesgoferreiro@tron-mainz.de>
2020-04-27 16:17:41 +0200

    move PROVEAN annotator to a class

--- input/tests/integration_tests/test_input.py
+++ input/tests/integration_tests/test_input.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from input.predict_all_epitopes import Bunchepitopes
+from input.predict_all_epitopes import BunchEpitopes
 
 
 class TestInput(TestCase):
@@ -18,7 +18,7 @@ class TestInput(TestCase):
         input_file = '/projects/SUMMIT/WP1.2/input/development/Pt29.sequences4testing.txt'
         alleles_file = '/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/vanallen/output_tables/20200106_alleles_extended.csv'
         tumor_content_file = '/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/vanallen/output_tables/vanallen_patient_overview.csv'
-        Bunchepitopes().wrapper_table_add_feature_annotation(
+        BunchEpitopes().wrapper_table_add_feature_annotation(
             file=input_file,
             indel=False,
             path_to_hla_file=alleles_file,
