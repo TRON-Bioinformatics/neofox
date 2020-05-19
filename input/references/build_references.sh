22fb3c1e365faf8a54ed6c1c20d74605e7c923d3
Lang <franlang@tronc6.zdv.uni-mainz.de>
2020-05-19 20:44:21 +0200

    generate reference files

--- input/references/build_references.sh
+++ input/references/build_references.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-# remove and adjust
-module load anaconda/3/2019
-INPUT_MIXMHC2PRED=/code/net/MixMHC2pred/1.1/MixMHC2pred
-INPUT_MAKEBLASTDB=/code/ncbi-blast/2.8.1+/bin/makeblastdb
-INPUT_RSCRIPT=/code/R/3.6.0/bin/Rscript
-path_to_references="/home/franlang/refs_test/"
-
 
 path_to_mixMHC2pred=`echo $INPUT_MIXMHC2PRED |sed 's/\(.*\)MixMHC2pred/\1/'`
 echo $path_to_mixMHC2pred
 
 # available MHC alleles netMHCpan
-/code/net/MHCpan/4.0/netMHCpan -listMHC | grep "HLA-" > "$path_to_references"/MHC_available.csv
+/code/net/MHCpan/4.0/netMHCpan -listMHC | grep "HLA-" > "$INPUT_REFERENCE_FOLDER"/MHC_available.csv
 
 
 # available MHCII alleles netMHCIIpan
-/code/net/MHCIIpan/3.2/netMHCIIpan -list  > "$path_to_references"/avail_mhcII.txt
+/code/net/MHCIIpan/3.2/netMHCIIpan -list  > "$INPUT_REFERENCE_FOLDER"/avail_mhcII.txt
 
 
 # available MHCII alleles for MixMHC2pred
-cp "$path_to_mixMHC2pred"/Alleles_list.txt $path_to_references
+cp "$path_to_mixMHC2pred"/Alleles_list.txt $INPUT_REFERENCE_FOLDER
 
 # build IEDB blast database
-mkdir "$path_to_references"/iedb
-wget "http://www.iedb.org/downloader.php?file_name=doc/tcell_full_v3.zip" -O "$path_to_references"/iedb/Iedb.zip
-unzip "$path_to_references"/iedb/Iedb.zip -d "$path_to_references"/iedb/
-$INPUT_RSCRIPT "$path_to_references"/build_IEDB_db.R "$path_to_references"/iedb/
-$INPUT_MAKEBLASTDB -in "$path_to_references"/iedb/IEDB.fasta -dbtype prot -parse_seqids -out "$path_to_references"/iedb/iedb
+mkdir "$INPUT_REFERENCE_FOLDER"/iedb
+wget "http://www.iedb.org/downloader.php?file_name=doc/tcell_full_v3.zip" -O "$INPUT_REFERENCE_FOLDER"/iedb/Iedb.zip
+unzip "$INPUT_REFERENCE_FOLDER"/iedb/Iedb.zip -d "$INPUT_REFERENCE_FOLDER"/iedb/
+$INPUT_RSCRIPT "$INPUT_REFERENCE_FOLDER"/build_IEDB_db.R "$INPUT_REFERENCE_FOLDER"/iedb/
+$INPUT_MAKEBLASTDB -in "$INPUT_REFERENCE_FOLDER"/iedb/IEDB.fasta -dbtype prot -parse_seqids -out "$INPUT_REFERENCE_FOLDER"/iedb/iedb
 
 # human proteome database
-mkdir "$path_to_references"/proteom_db
-wget ftp://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz -O "$path_to_references"/proteom_db/Homo_sapiens.fa.gz
-gunzip "$path_to_references"/proteom_db/Homo_sapiens.fa.gz
-$INPUT_MAKEBLASTDB -in "$path_to_references"/proteom_db/Homo_sapiens.fa -dbtype prot -parse_seqids -out "$path_to_references"/proteom_db/homo_sapiens
+mkdir "$INPUT_REFERENCE_FOLDER"/proteom_db
+wget ftp://ftp.ensembl.org/pub/release-100/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz -O "$INPUT_REFERENCE_FOLDER"/proteom_db/Homo_sapiens.fa.gz
+gunzip "$INPUT_REFERENCE_FOLDER"/proteom_db/Homo_sapiens.fa.gz
+$INPUT_MAKEBLASTDB -in "$INPUT_REFERENCE_FOLDER"/proteom_db/Homo_sapiens.fa -dbtype prot -parse_seqids -out "$INPUT_REFERENCE_FOLDER"/proteom_db/homo_sapiens
 
 # use Homo_sapiens.fa as uniprot_human_with_isoforms.fasta to be consistent with human proteome database
 # provide BLOSUM62-2.matrix.txt as file and refer to "https://arxiv.org/abs/1205.6031"
