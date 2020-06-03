df7f77e1c09ec4aa8b9bb8b2bd61a271c1b6315d
Pablo Riesgo Ferreiro <pablo.riesgoferreiro@tron-mainz.de>
2020-06-03 22:50:05 +0200

    remove duplicated code for MixMHCpred

--- input/MixMHCpred/mixmhcpred.py
+++ input/MixMHCpred/mixmhcpred.py
@@ -2,10 +2,11 @@
 
 from logzero import logger
 
-from input.helpers import properties_manager, intermediate_files
+from input.MixMHCpred.abstract_mixmhcpred import AbstractMixMHCpred
+from input.helpers import intermediate_files
 
 
-class MixMHCpred:
+class MixMHCpred(AbstractMixMHCpred):
 
     def __init__(self, runner, configuration):
         """
@@ -27,40 +28,6 @@ class MixMHCpred:
         self.best_rank_wt = "NA"
         self.difference_score_mut_wt = "NA"
 
-    def mut_position_xmer_seq(self, xmer_wt, xmer_mut):
-        '''returns position of mutation in xmer sequence
-        '''
-        p1 = -1
-        if len(xmer_wt) == len(xmer_mut):
-            p1 = -1
-            for i, aa in enumerate(xmer_mut):
-                if aa != xmer_wt[i]:
-                    p1 = i + 1
-        else:
-            p1 = 0
-            # in case sequences do not have same length
-            for a1, a2 in zip(xmer_wt, xmer_mut):
-                if a1 == a2:
-                    p1 += 1
-        return str(p1)
-
-    def generate_nmers(self, xmer_wt, xmer_mut, list_lengths):
-        ''' generates peptides covering mutation of all lengts that are provided. Returns peptides as list
-        '''
-        list_peptides = []
-        pos_mut = int(self.mut_position_xmer_seq(xmer_wt=xmer_wt, xmer_mut=xmer_mut))
-        for l in list_lengths:
-            l = int(l)
-            start_first = pos_mut - (l)
-            starts = []
-            for s in range(l):
-                starts.append(int(start_first + s))
-            ends = []
-            [ends.append(int(s + (l))) for s in starts]
-            for s, e in zip(starts, ends):
-                list_peptides.append(xmer_mut[s:e])
-        return list_peptides
-
     def mixmhcprediction(self, hla_alleles, tmpfasta, outtmp):
         ''' Performs MixMHCpred prediction for desired hla allele and writes result to temporary file.
         '''
@@ -75,26 +42,6 @@ class MixMHCpred:
             "-a", hla_allele,
             "-i", tmpfasta,
             "-o", outtmp])
-
-    def read_mixmhcpred(self, outtmp):
-        '''imports output of MixMHCpred prediction
-        '''
-        counter = 0
-        header = []
-        dat = []
-        with open(outtmp) as f:
-            for line in f:
-                line = line.rstrip().lstrip()
-                if line:
-                    if line.startswith("#"):
-                        continue
-                    if line.startswith("Peptide"):
-                        counter += 1
-                        header = line.split()
-                        continue
-                    line = line.split()
-                    dat.append(line)
-        return header, dat
 
     def extract_best_per_pep(self, pred_dat):
         '''extract info of best allele prediction for all potential ligands per muatation
@@ -142,37 +89,6 @@ class MixMHCpred:
         head_new = ["Peptide", "Score_bestAllele", "%Rank_bestAllele", "BestAllele"]
         return head_new, min_pep
 
-    def add_best_epitope_info(self, epitope_tuple, column_name):
-        '''returns desired information of prediction of best epitope from netmhcpan output;
-        e.g. "%Rank": MHC I score, "HLA": HLA allele, "Icore": best epitope
-        '''
-        dat_head = epitope_tuple[0]
-        dat = epitope_tuple[1]
-        val = dat_head.index(column_name)
-        try:
-            return dat[val]
-        except IndexError:
-            return "NA"
-
-    def extract_WT_for_best(self, xmer_wt, xmer_mut, best_mut_seq):
-        '''extracts the corresponding WT epitope for best predicted mutated epitope
-        '''
-        start = xmer_mut.find(best_mut_seq)
-        l = len(best_mut_seq)
-        wt_epi = xmer_wt[start:(start + l)]
-        return (wt_epi)
-
-    def extract_WT_info(self, epitope_tuple, column_name):
-        '''
-        '''
-        dat_head = epitope_tuple[0]
-        dat = epitope_tuple[1][0]
-        val = dat_head.index(column_name)
-        try:
-            return dat[val]
-        except IndexError:
-            return "NA"
-
     def difference_score(self, mut_score, wt_score):
         ''' calcualated difference in MixMHCpred scores between mutated and wt
         '''
@@ -185,7 +101,7 @@ class MixMHCpred:
         '''Wrapper for MHC binding prediction, extraction of best epitope and check if mutation is directed to TCR
         '''
         tmp_prediction = intermediate_files.create_temp_file(prefix="mixmhcpred", suffix=".txt")
-        seqs = self.generate_nmers(xmer_wt=xmer_wt, xmer_mut=xmer_mut, list_lengths=[8, 9, 10, 11])
+        seqs = self.generate_nmers(xmer_wt=xmer_wt, xmer_mut=xmer_mut, lengths=[8, 9, 10, 11])
         tmp_fasta = intermediate_files.create_temp_fasta(seqs, prefix="tmp_sequence_")
         self.mixmhcprediction(alleles, tmp_fasta, tmp_prediction)
         pred = self.read_mixmhcpred(tmp_prediction)
@@ -219,36 +135,3 @@ class MixMHCpred:
             self.best_rank_wt = self.extract_WT_info(pred_wt, rank_wt_of_interest)
             # difference in scores between mut and wt
             self.difference_score_mut_wt = self.difference_score(self.best_score, self.best_score_wt)
-
-
-# if __name__ == '__main__':
-#
-#     from input import predict_all_epitopes, epitope
-#
-#     # test with ott data set
-#     file = "/projects/CM01_iVAC/immunogenicity_prediction/3rd_party_solutions/MHC_prediction_netmhcpan4/testdat_ott.txt"
-#     hla_file = "/projects/SUMMIT/WP1.2/Literature_Cohorts/data_analysis/cohorts/ott/icam_ott/alleles.csv"
-#     # test inest data set
-#     # file = "/flash/projects/WP3/AnFranziska/AnFranziska/head_seqs.txt"
-#     # hla_file = "/flash/projects/WP3/AnFranziska/AnFranziska/alleles.csv"
-#     dat = data_import.import_dat_icam(file, False)
-#     if "+-13_AA_(SNV)_/_-15_AA_to_STOP_(INDEL)" in dat[0]:
-#         dat = data_import.change_col_names(dat)
-#     # available MHC alleles
-#     set_available_mhc = predict_all_epitopes.Bunchepitopes().load_available_hla_alleles()
-#     # hla allele of patients
-#     patient_hlaI = predict_all_epitopes.Bunchepitopes().load_patient_hla_I_allels(hla_file)
-#     patient_hlaII = predict_all_epitopes.Bunchepitopes().load_patient_hla_II_allels(hla_file)
-#
-#     print(patient_hlaI)
-#     print(patient_hlaII)
-#
-#     for ii, i in enumerate(dat[1]):
-#         if ii < 10:
-#             print(ii)
-#             dict_epi = epitope.Epitope()
-#             dict_epi.init_properties(dat[0], dat[1][ii])
-#             prediction = MixMHCpred()
-#             prediction.main(dict_epi.properties, patient_hlaI)
-#             attrs = vars(prediction)
-#             print(attrs)
