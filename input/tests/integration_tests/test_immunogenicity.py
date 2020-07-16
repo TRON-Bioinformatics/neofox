83299b5a7b36eb6bb7474fe19a927ad4a9c4d755
Lang <franlang@tronc6.zdv.uni-mainz.de>
2020-07-16 15:36:06 +0200

    correction

--- input/tests/integration_tests/test_immunogenicity.py
+++ input/tests/integration_tests/test_immunogenicity.py
@@ -3,16 +3,16 @@ from unittest import TestCase
 from input.IEDB_Immunogenicity.predict_immunogenicity_simple import IEDBimmunogenicity
 
 
-class TestImmunogenicity(TestCase)
+class TestImmunogenicity(TestCase):
 
     def setUp(self):
         self.immunogenicity_calculator = IEDBimmunogenicity()
 
     def test_immunogenicity(self):
-        result = self.immunogenicity_calculator.calc_IEDB_immunogenicity(epitope="NLVPMVATV",
+        result = self.immunogenicity_calculator.calc_IEDB_immunogenicity(epitope="ENPVVHFF",
                                                                          mhc_allele="HLA-A*68:01", mhc_score=600)
         self.assertGreater(result, 0)
-        result = self.immunogenicity_calculator.calc_IEDB_immunogenicity(epitope="NLVPMVATV",
+        result = self.immunogenicity_calculator.calc_IEDB_immunogenicity(epitope="ENPVVHFF",
                                                                          mhc_allele="HLA-A*68:01", mhc_score=600,
                                                                          affin_filtering=True)
-        self.assertGreater(result, "NA")
+        self.assertEqual(result, "NA")
