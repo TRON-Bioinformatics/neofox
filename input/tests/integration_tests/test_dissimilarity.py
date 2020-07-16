d89c22ffd7b0590677007e6fac96185292caf0d8
Lang <franlang@tronc6.zdv.uni-mainz.de>
2020-07-16 14:23:29 +0200

    add 15mer with dissimilarity for current to integration test

--- input/tests/integration_tests/test_dissimilarity.py
+++ input/tests/integration_tests/test_dissimilarity.py
@@ -34,9 +34,9 @@ class TestDissimilarity(TestCase):
             mhc_mutation='LGLSDSQFLQTFLFM', mhc_affinity='430', references=self.references)
         self.assertEqual(result, 0)
         # unsimmilar peptide
-        # TODO: change this peptide sequence by 15mer when parameter a was trained for MHC class II 
         result = DissimilarityCalculator(
             runner=self.runner, configuration=self.configuration) \
             .calculate_dissimilarity(
-            mhc_mutation='QLEQALDCEIY', mhc_affinity='430', references=self.references)
+            mhc_mutation='LFTSPIMTKSAEMIV', mhc_affinity='430', references=self.references)
+        print(result)
         self.assertGreater(result, 0.0)
