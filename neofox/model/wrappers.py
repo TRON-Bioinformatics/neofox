f84aa094fb90c710b78ef7c30e8f3d945b4ed1b7
priesgof <priesgoferreiro@gmail.com>
2020-09-08 21:57:39 +0200

    limit precision

--- neofox/model/wrappers.py
+++ neofox/model/wrappers.py
@@ -11,7 +11,10 @@ class AnnotationFactory(object):
             # prints booleans as 0/1 strings
             value = "1" if value else "0"
         if not isinstance(value, str) and not isinstance(value, type(None)):
-            value = str(value)
+            if isinstance(value, float):
+                value = "{0:.5g}".format(round(value, 5))
+            else:
+                value = str(value)
         if value is None:
             value = NOT_AVAILABLE_VALUE
         return Annotation(name=name, value=value)
