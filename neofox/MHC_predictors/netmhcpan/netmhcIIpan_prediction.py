47630b2594b6d16a455cd72de06098cd2fd7d2c9
Lang <franlang@tronc7.zdv.uni-mainz.de>
2020-09-08 13:21:39 +0200

    some renaming

--- neofox/MHC_predictors/netmhcpan/netmhcIIpan_prediction.py
+++ neofox/MHC_predictors/netmhcpan/netmhcIIpan_prediction.py
@@ -32,8 +32,9 @@ class NetMhcIIPanPredictor(EpitopeHelper, AbstractNetMhcPanPredictor):
         return allele_correct
 
     def generate_mhcII_alelles_combination_list(self, hla_alleles, set_available_mhc):
-        ''' given list of HLA II alleles, returns list of HLA-DRB1 (2x), all possible HLA-DPA1/HLA-DPB1 (4x) and HLA-DQA1/HLA-DPQ1 (4x)
-        '''
+        """ given list of HLA II alleles, returns list of HLA-DRB1 (2x), all possible HLA-DPA1/HLA-DPB1 (4x)
+        and HLA-DQA1/HLA-DPQ1 (4x)
+        """
         allels_for_prediction = []
         dqa_alleles = []
         dpa_alleles = []
@@ -66,8 +67,8 @@ class NetMhcIIPanPredictor(EpitopeHelper, AbstractNetMhcPanPredictor):
         return allels_for_prediction
 
     def mhcII_prediction(self, hla_alleles, set_available_mhc, tmpfasta, tmppred):
-        ''' Performs netmhcIIpan prediction for desired hla alleles and writes result to temporary file.
-        '''
+        """ Performs netmhcIIpan prediction for desired hla alleles and writes result to temporary file.
+        """
         alleles_for_prediction = self.generate_mhcII_alelles_combination_list(hla_alleles, set_available_mhc)
         hla_allele = ",".join(alleles_for_prediction)
         tmp_folder = tempfile.mkdtemp(prefix="tmp_netmhcIIpan_")
@@ -102,46 +103,46 @@ class NetMhcIIPanPredictor(EpitopeHelper, AbstractNetMhcPanPredictor):
         filters prediction file for predicted epitopes that cover mutations
         """
         header, data = data_import.import_dat_general(tmppred)
-        dat_fil = []
+        epitopes_covering_mutation = []
         pos_epi = header.index("Seq")
         epi = header.index("Peptide")
         for ii, i in enumerate(data):
             if self.epitope_covers_mutation(position_xmer_sequence, i[pos_epi], len(i[epi])):
-                dat_fil.append(data[ii])
-        return header, dat_fil
+                epitopes_covering_mutation.append(data[ii])
+        return header, epitopes_covering_mutation
 
     def minimal_binding_score(self, prediction_tuple, rank=True):
-        '''reports best predicted epitope (over all alleles). indicate by rank = true if rank score should be used. if rank = False, Aff(nM) is used
-        '''
+        """reports best predicted epitope (over all alleles). indicate by rank = true if rank score should be used.
+        if rank = False, Aff(nM) is used
+        """
         # TODO: generalize this method with netmhcpan_prediction.py + change neofox
-        dat_head = prediction_tuple[0]
-        dat = prediction_tuple[1]
+        header = prediction_tuple[0]
+        data = prediction_tuple[1]
         if rank:
-            mhc_sc = dat_head.index("%Rank")
+            mhc_sc = header.index("%Rank")
         else:
-            mhc_sc = dat_head.index("Affinity(nM)")
+            mhc_sc = header.index("Affinity(nM)")
         max_score = float(1000000000)
-        row = []
-        for ii, i in enumerate(dat):
+        best_predicted_epitope = []
+        for ii, i in enumerate(data):
             mhc_score = float(i[mhc_sc])
             if mhc_score < max_score:
                 max_score = mhc_score
-                row = i
-        return dat_head, row
+                best_predicted_epitope = i
+        return header, best_predicted_epitope
 
     def filter_for_wt_epitope_position(self, prediction_tuple, mut_seq, position_epi_xmer):
-        '''returns wt epitope info for given mutated sequence. best wt that is allowed to bind to any allele of patient
-        '''
-        dat_head = prediction_tuple[0]
-        dat = prediction_tuple[1]
-        seq_col = dat_head.index("Peptide")
-        pos_col = dat_head.index("Seq")
-        wt_epi = []
-        for ii, i in enumerate(dat):
+        """returns wt epitope info for given mutated sequence. best wt that is allowed to bind to any allele of patient
+        """
+        header = prediction_tuple[0]
+        data = prediction_tuple[1]
+        seq_col = header.index("Peptide")
+        pos_col = header.index("Seq")
+        epitopes_wt = []
+        for ii, i in enumerate(data):
             wt_seq = i[seq_col]
             wt_pos = i[pos_col]
             if (len(wt_seq) == len(mut_seq)) & (wt_pos == position_epi_xmer):
-                wt_epi.append(i)
-        dt = (dat_head, wt_epi)
-        min = self.minimal_binding_score(dt)
-        return (min)
+                epitopes_wt.append(i)
+        all_epitopes_wt = (header, epitopes_wt)
+        return self.minimal_binding_score(all_epitopes_wt)
