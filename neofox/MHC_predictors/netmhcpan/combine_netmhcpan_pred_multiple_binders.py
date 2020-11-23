938ca2dccdf1f70ca0274dec53e1b8cce45a0bce
priesgof <priesgoferreiro@gmail.com>
2020-11-23 14:50:02 +0100

    corrects the selection of WT best epitope by affinity

--- neofox/MHC_predictors/netmhcpan/combine_netmhcpan_pred_multiple_binders.py
+++ neofox/MHC_predictors/netmhcpan/combine_netmhcpan_pred_multiple_binders.py
@@ -152,27 +152,19 @@ class BestAndMultipleBinder:
             position_of_mutation=position_of_mutation, predictions=predictions_wt)
 
         # best prediction
-        self.best_wt_epitope_by_rank = netmhcpan.filter_for_WT_epitope_position(
-            filtered_predictions_wt, self.best_epitope_by_rank.peptide,
-            position_mutation_epitope=self.best_epitope_by_rank.pos)
-
-        # TODO: careful here, this is computed against the best epitopes by affinity but then the best is chosen based
-        # TODO: on the rank. Review!!
-        self.best_wt_epitope_by_affinity = netmhcpan.filter_for_WT_epitope_position(
-            filtered_predictions_wt, self.best_epitope_by_affinity.peptide,
-            position_mutation_epitope=self.best_epitope_by_affinity.pos)
+        self.best_wt_epitope_by_rank = netmhcpan.select_best_by_rank(
+            netmhcpan.filter_WT_predictions_from_best_mutated(filtered_predictions_wt, self.best_epitope_by_rank))
+        self.best_wt_epitope_by_affinity = netmhcpan.select_best_by_affinity(
+            netmhcpan.filter_WT_predictions_from_best_mutated(filtered_predictions_wt, self.best_epitope_by_affinity))
 
         # best predicted epitope of length 9
         ninemer_predictions_wt = netmhcpan.filter_for_9mers(filtered_predictions_wt)
-        self.best_ninemer_wt_epitope_by_rank = netmhcpan.filter_for_WT_epitope_position(
-            ninemer_predictions_wt, sequence_mut=self.best_ninemer_epitope_by_rank.peptide,
-            position_mutation_epitope=self.best_ninemer_epitope_by_rank.pos)
-
-        # TODO: careful here, this is computed against the best epitopes by affinity but then the best is chosen based
-        # TODO: on the rank. Review!!
-        self.best_ninemer_wt_epitope_by_affinity = netmhcpan.filter_for_WT_epitope_position(
-            ninemer_predictions_wt, sequence_mut=self.best_ninemer_epitope_by_affinity.peptide,
-            position_mutation_epitope=self.best_ninemer_epitope_by_affinity.pos)
+        self.best_ninemer_wt_epitope_by_rank = netmhcpan.select_best_by_rank(
+            netmhcpan.filter_WT_predictions_from_best_mutated(
+                ninemer_predictions_wt, self.best_ninemer_epitope_by_rank))
+        self.best_ninemer_wt_epitope_by_affinity = netmhcpan.select_best_by_affinity(
+            netmhcpan.filter_WT_predictions_from_best_mutated(
+                ninemer_predictions_wt, self.best_ninemer_epitope_by_affinity))
 
     def get_annotations(self) -> List[Annotation]:
         annotations = [
