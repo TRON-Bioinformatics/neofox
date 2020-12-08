ef7a4f94681bb50faf099428917af247f5f7160d
Lang <franlang@tronc2.zdv.uni-mainz.de>
2020-12-08 19:14:20 +0100

    correct hla alllele pattern

--- neofox/model/wrappers.py
+++ neofox/model/wrappers.py
@@ -23,7 +23,7 @@ import re
 NOT_AVAILABLE_VALUE = "NA"
 
 HLA_ALLELE_PATTERN = re.compile(
-    r"(?:HLA-)?((?:A|B|C|DPA1|DPB1|DQA1|DQB1|DRB1))[\*|_]?([0-9]{2})[:|_]?([0-9]{2,3})[:|_]?([0-9]{2,})?[:|_]?([0-9]{2,})?([N|L|S|Q]{0,1})"
+    r"(?:HLA-)?((?:A|B|C|DPA1|DPB1|DQA1|DQB1|DRB1))[\*|_]?([0-9]{2,})[:|_]?([0-9]{2,3})[:|_]?([0-9]{2,})?[:|_]?([0-9]{2,})?([N|L|S|Q]{0,1})"
 )
 HLA_MOLECULE_PATTERN = re.compile(
     r"(?:HLA-)?((?:DPA1|DPB1|DQA1|DQB1|DRB1)[\*|_]?[0-9]{2,}[:|_]?[0-9]{2,})[-|_]{1,2}"
