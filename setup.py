db0a87cecf72d577ec5f41b9624b409bc41c5342
priesgof <priesgoferreiro@gmail.com>
2021-05-05 12:10:24 +0200

    accept py39.2 for testing

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: Unix"
     ],
-    python_requires='>=3.6,<=3.9',
+    python_requires='>=3.6,<=3.9.2',
     license='GPLv3',
 )
