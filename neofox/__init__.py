68785f057bce0c6a8fc05632427be24c9f040ec8
Lang <franlang@tronlogin.zdv.uni-mainz.de>
2021-07-13 12:30:49 +0200

    increase version

--- neofox/__init__.py
+++ neofox/__init__.py
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.#
-VERSION = "0.5.1.dev4"
+VERSION = "0.5.2.dev0"
 
 
 REFERENCE_FOLDER_ENV = "NEOFOX_REFERENCE_FOLDER"
