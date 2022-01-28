40bcad39fc27e23781346b4dd9f6fa5eb5c69fe9
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-01-28 16:07:15 +0100

    bump version

--- neofox/__init__.py
+++ neofox/__init__.py
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.#
-VERSION = "0.5.4.dev11"
+VERSION = "0.5.4.dev12"
 
 
 REFERENCE_FOLDER_ENV = "NEOFOX_REFERENCE_FOLDER"
