4602447cc8e60fe667cd081a7edf41db88a9be21
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-08-03 11:12:36 +0200

    fix netmhcpan test

--- neofox/tests/integration_tests/test_netmhcpan.py
+++ neofox/tests/integration_tests/test_netmhcpan.py
@@ -44,11 +44,9 @@ class TestNetMhcPanPredictor(TestCase):
         )
         # this is an epitope from IEDB of length 9
         mutated = "NLVPMVATV"
-        predictions = netmhcpan_predictor.mhc_prediction(
-            sequence=mutated,
-            mhc_alleles=self.test_mhc_one,
-            set_available_mhc=self.available_alleles.get_available_mhc_i(),
-        )
+        available_alleles = netmhcpan_predictor.get_only_available_alleles(
+            mhc_alleles=self.test_mhc_one, set_available_mhc=self.available_alleles.get_available_mhc_i())
+        predictions = netmhcpan_predictor.mhc_prediction(available_alleles=available_alleles, sequence=mutated)
         self.assertEqual(18, len(predictions))
 
     def test_netmhcpan_too_small_epitope(self):
@@ -57,11 +55,9 @@ class TestNetMhcPanPredictor(TestCase):
             blastp_runner=self.proteome_blastp_runner
         )
         mutated = "NLVP"
-        predictions = netmhcpan_predictor.mhc_prediction(
-            sequence=mutated,
-            mhc_alleles=self.test_mhc_one,
-            set_available_mhc=self.available_alleles.get_available_mhc_i(),
-        )
+        available_alleles = netmhcpan_predictor.get_only_available_alleles(
+            mhc_alleles=self.test_mhc_one, set_available_mhc=self.available_alleles.get_available_mhc_i())
+        predictions = netmhcpan_predictor.mhc_prediction(sequence=mutated, available_alleles=available_alleles)
         self.assertEqual(0, len(predictions))
 
     def test_netmhcpan_rare_aminoacid(self):
@@ -71,11 +67,9 @@ class TestNetMhcPanPredictor(TestCase):
         )
         # this is an epitope from IEDB of length 9
         mutated = "XTTDSWGKF"
-        predictions = netmhcpan_predictor.mhc_prediction(
-            sequence=mutated,
-            mhc_alleles=self.test_mhc_one,
-            set_available_mhc=self.available_alleles.get_available_mhc_i(),
-        )
+        available_alleles = netmhcpan_predictor.get_only_available_alleles(
+            mhc_alleles=self.test_mhc_one, set_available_mhc=self.available_alleles.get_available_mhc_i())
+        predictions = netmhcpan_predictor.mhc_prediction(sequence=mutated, available_alleles=available_alleles)
         self.assertEqual(18, len(predictions))
 
     def test_netmhc2pan_epitope_iedb(self):
