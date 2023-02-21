65110ece8e313bf8a0a64157a79591b82ea4e5e6
nguyenhv <van.nguyen-hoang@tron-mainz.de>
2023-02-21 16:07:51 +0100

    257-fix error in command_line

--- neofox/command_line.py
+++ neofox/command_line.py
@@ -217,7 +217,7 @@ def _read_data(input_file, patients_data, mhc_database: MhcDatabase) -> Tuple[Li
     neoantigens_patient_ids = set(i['patientIdentifier'] for i in neoantigens)
     patient_ids = set(j['identifier'] for j in patients)
     if neoantigens_patient_ids.difference(patient_ids) > 0:
-        raise ValueError('%s patient candidate does not exist in the patient data file.' % patient)
+        raise ValueError('%s patient candidate does not exist in the patient data file.' % neoantigens_patient_ids)
 
     return neoantigens, patients
 
