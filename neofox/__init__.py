4d51580378c616b25897f2d23c60ed238731e35b
Luis Kress <luis.kress@tron-mainz.de>
2023-04-04 16:33:56 +0200

    Updated version

--- neofox/__init__.py
+++ neofox/__init__.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.#
 
 
-VERSION = "1.1.0b13"
+VERSION = "1.1.0b15"
 
 REFERENCE_FOLDER_ENV = "NEOFOX_REFERENCE_FOLDER"
 NEOFOX_BLASTP_ENV = "NEOFOX_BLASTP"
