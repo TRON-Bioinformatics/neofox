085431621783ee9a91c8bc9611efb111f3a052e3
Luis Kress <luis.kress@tron-mainz.de>
2024-01-12 11:55:57 +0100

    Cleaned up vaxrank integration tests

--- neofox/tests/integration_tests/integration_test_tools.py
+++ neofox/tests/integration_tests/integration_test_tools.py
@@ -107,61 +107,6 @@ def get_h2_two_test_b(h2_database):
         ], h2_database
     )
 
-def get_test_epitopes(hla_database):
-    # Epitopes of the neoantigen DSVVIFSGEGSDEFTQGYIYFHKAPSP
-    # for testing VaxRank
-    affinity_values = [1.74, 7.67, 802.78, 2057.1, 770.35, 5002.44]
-    
-    neoepitopes = [
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "VIFSGEGSDEF",
-            wild_type_peptide = "VIFSGEGSDEL",
-            allele_mhc_i = "HLA-A*24:02",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "VIFSGEGSDEF",
-            wild_type_peptide = "VIFSGEGSDEL",
-            allele_mhc_i = "HLA-A*02:01",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "VIFSGEGSDEF",
-            wild_type_peptide = "VIFSGEGSDEL",
-            allele_mhc_i = "HLA-B*15:01",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "IFSGEGSDEFT",
-            wild_type_peptide = "IFSGEGSDELT",
-            allele_mhc_i = "HLA-A*24:02",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "IFSGEGSDEFT",
-            wild_type_peptide = "IFSGEGSDELT",
-            allele_mhc_i = "HLA-A*02:01",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-        # this epitope will not be considered for vaxrank calculation
-        NeoepitopeFactory.build_neoepitope(
-            mutated_peptide = "IFSGEGSDEFT",
-            wild_type_peptide = "IFSGEGSDELT",
-            allele_mhc_i = "HLA-B*15:01",
-            mhc_database = hla_database,
-            organism = "human"
-        ),
-    ]
-    
-    for i, a in enumerate(affinity_values):
-        neoepitopes[i].affinity_mutated = affinity_values[i]
-
-    return neoepitopes
 
 mutations_with_rare_aminoacids = [
             ("UTTDSDGKF", "UTTDSWGKF"),  # this is an epitope from IEDB of length 9
--- neofox/tests/integration_tests/test_vaxrank.py
+++ neofox/tests/integration_tests/test_vaxrank.py
@@ -20,47 +20,86 @@ import os
 from unittest import TestCase
 
 import neofox.tests.integration_tests.integration_test_tools as integration_test_tools
+from neofox.model.factories import AnnotationFactory
 
 from neofox.helpers.runner import Runner
 from neofox.model.neoantigen import PredictedEpitope
 from neofox.published_features.vaxrank.vaxrank import (
     VaxRank,
 )
+from neofox.tests.fake_classes import FakePredictedEpitope
 
+# TODO: maybe create Fake Class Predicted Epitope to easier create test epitopes
 
 class TestVaxRank(TestCase):
     def setUp(self):
         self.references, self.configuration = integration_test_tools.load_references()
         self.runner = Runner()
         self.vaxrank = VaxRank()
+        self.AnnotationFactory = AnnotationFactory()
         self.hla_database = self.references.get_mhc_database()
+
+        # Generate epitopes for test case
+        # NOTE: these should remain the same, to check if something in the 
+        #       vaxrank calculation has changed
+        epitopes = [
+            "VIFSGEGSDEF", "VIFSGEGSDEF", "VIFSGEGSDEF", "IFSGEGSDEFT", "IFSGEGSDEFT", "IFSGEGSDEFT"
+        ]
+        alleles = [
+            "HLA-A*24:02", "HLA-A*02:01", "HLA-B*15:01", "HLA-A*24:02", "HLA-A*02:01", "HLA-B*15:01"
+        ]
+        affinities = [1.74, 7.67, 802.78, 2057.1, 770.35, 5002.44]
+        self.pred_epitopes = [FakePredictedEpitope(e, m, a) for e, m, a in zip(epitopes, alleles, affinities)]
+
+        # expected binding value
+        self.TRUE_BINDING_SCORE = 2.1082247484257532
+
+    def test_vaxrank_binding(self):
+        """
+        Test if the VaxRank binding score is calculated correctly.
+
+        The binding score of one list of neoepitopes is calculated and compared
+        with the manually calculated value. As floating point calculations can 
+        differ between architectures and the vaxrank annotations are rounded, we 
+        only compare on almost equal.
+        """
+
+        binding_score = self.vaxrank.total_binding(self.pred_epitopes)
+
+        self.assertAlmostEqual(binding_score, self.TRUE_BINDING_SCORE)
 
     def test_vaxrank(self):
         """
         Test whether the VaxRank calculation works correct. 
 
-        The binding score of one list of neoepitopes is calculated and compared
-        with the true value. As floating point calculations can differ between 
-        architectures and the vaxrank annotations are rounded, we only compare on 
-        almost equal.
-        Additionaly it is checked if the total score and the total imputed score
-        is correctly calculated.
+        It is checked if the total score and the total imputed score is correctly calculated.
         """
         EXPRESSION_SCORE = 10.4
         IMPUTED_SCORE = 7.6
 
-        neoepitopes = integration_test_tools.get_test_epitopes(self.hla_database)
+        binding_score = self.vaxrank.total_binding(self.pred_epitopes)
 
-        annot = VaxRank().get_annotations(
-            epitope_predictions = neoepitopes,
+        # expected annotation
+        expected_total_score = AnnotationFactory.build_annotation(
+                value=EXPRESSION_SCORE * binding_score, 
+                name="Vaxrank_totalScore"
+            )
+        expected_total_score_imputed = AnnotationFactory.build_annotation(
+                value=IMPUTED_SCORE * binding_score, 
+                name="Vaxrank_totalScore_imputed"
+            )
+
+        # calculated annotation
+        annot = self.vaxrank.get_annotations(
+            epitope_predictions = self.pred_epitopes,
             expression_score = EXPRESSION_SCORE, 
             imputed_score = IMPUTED_SCORE
             )
 
-        # to access the values of the Vaxrank annotation even if the order changes
-        annot_dict = {a.name: float(a.value) for a in annot}
+        # access the values of the Vaxrank annotation even if the order changes
+        annot_dict = {a.name: a.value for a in annot}
 
-        # the binding score calculated by hand
-        assert abs(annot_dict["Vaxrank_bindingScore"] - 2.108220805) < 0.001
-        assert abs(annot_dict["Vaxrank_bindingScore"] * EXPRESSION_SCORE - annot_dict["Vaxrank_totalScore"]) < 0.001
-        assert abs(annot_dict["Vaxrank_bindingScore"] * IMPUTED_SCORE - annot_dict["Vaxrank_totalScore_imputed"]) < 0.001
+        # since strings with the rounded value are returned by the get_annotations
+        # function, we can compare here on equality
+        assert annot_dict["Vaxrank_totalScore"] == expected_total_score.value
+        assert annot_dict["Vaxrank_totalScore_imputed"] == expected_total_score_imputed.value
